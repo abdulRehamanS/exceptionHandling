d0aad8e188bc5dd2d78a4b8f6a90fb4025a68cfe
abdulRehamanS <abdulrehaman627@gmail.com>
2023-07-30 16:39:10 +0530

    adding clear notes to project

--- Exception Handling/Exception-Handling/src/main/java/In/abdul/Exception/Handling/ExceptionHandling/AppException.java
+++ Exception Handling/Exception-Handling/src/main/java/In/abdul/Exception/Handling/ExceptionHandling/AppException.java
@@ -1,5 +1,9 @@
 package In.abdul.Exception.Handling.ExceptionHandling;
 
+
+
+
+// creating costume exception class here by extending to RunTimeException
 public class AppException extends RuntimeException {
 
 
--- Exception Handling/Exception-Handling/src/main/java/In/abdul/Exception/Handling/ExceptionHandling/ExceptionHandling.java
+++ Exception Handling/Exception-Handling/src/main/java/In/abdul/Exception/Handling/ExceptionHandling/ExceptionHandling.java
@@ -6,11 +6,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+@RestControllerAdvice  //RestControllerAdvice
 public class ExceptionHandling {
 
 
-    @ExceptionHandler(value = AppException.class)
+    @ExceptionHandler(value = AppException.class) // handling exception by cutsome exception class
     public ResponseEntity<ExceptionInfo> handleException(AppException app) {
 
         String msg = app.getMessage();
@@ -18,7 +18,6 @@ public class ExceptionHandling {
         ExceptionInfo info = new ExceptionInfo();
         info.setExceptionCode("EX200OK");
         info.setExceptionMessage("Exception occurred");
-
 
         return new ResponseEntity<>(info, HttpStatus.INTERNAL_SERVER_ERROR);
     }
